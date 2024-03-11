c491dcbe4b7a2be29dcd101c2508fac3e2bc6947
KWAGALA-JOANNA <142006038+KWAGALA-JOANNA@users.noreply.github.com>
2024-03-11 16:38:16 +0300

    TEST ONE.sql

--- TEST ONE.sql
+++ TEST ONE.sql
@@ -15,22 +15,22 @@ VALUES 		('1/2016', 'John', 'Mutuku', 'DCS', 'Male', 13/6/1990),
 			 
 SELECT * FROM students;
 		 
--- SELECT Reg_no, Last_Name, First_Name FROM students;
+SELECT Reg_no, Last_Name, First_Name FROM students;
 
 -- combining first name and last name in student name
 
 SELECT *, CONCAT(Last_Name,  First_Name) AS Student_Name FROM students;
 
--- SELECT DISTINCT gender, D_O_B, Last_Name, First_Name FROM students
--- WHERE course = 'DCS';
+SELECT DISTINCT gender, D_O_B, Last_Name, First_Name FROM students
+WHERE course = 'DCS';
 
 --Produce total number of students per course
--- SELECT SUM(Reg_no) FROM students
--- WHERE course = 'DCS' ;
--- SELECT SUM(Reg_no) FROM students
--- WHERE course = 'CIT' ;
--- SELECT SUM(Reg_no) FROM students
--- WHERE course = 'DBIT' ;
+SELECT SUM(Reg_no) FROM students
+WHERE course = 'DCS' ;
+SELECT SUM(Reg_no) FROM students
+WHERE course = 'CIT' ;
+SELECT SUM(Reg_no) FROM students
+WHERE course = 'DBIT' ;
 
 --List of all male students born between 1980 and 1989
 -- Modifying the last name
